378b04ec95b654c3577e8216d7b22ed7722eab48
lyday <dustin.lyday@gmail.com>
2016-04-19 18:48:11 -0500

    wired in knockoutjs

--- aspnet5/App_Start/BundleConfig.cs
+++ aspnet5/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace aspnet5
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockoutjs").Include(
+                        "~/Scripts/knockout-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
--- aspnet5/Scripts/HelloDustin.js
+++ aspnet5/Scripts/HelloDustin.js
@@ -1,2 +1,17 @@
-﻿alert("Hi Dustin");
+﻿$(function() {
 
+    // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+    function AppViewModel() {
+        this.firstName = "Bert";
+        this.lastName = "Bertington";
+
+        this.capitalizeLastName = function () {
+            var currentVal = this.lastName();        // Read the current value
+            this.lastName(currentVal.toUpperCase()); // Write back a modified value
+        };
+    }
+
+    // Activates knockout.js
+    ko.applyBindings(new AppViewModel());
+
+});
