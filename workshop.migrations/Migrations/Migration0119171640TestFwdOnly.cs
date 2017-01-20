28af69a94a46c832d09ec3e21879e4da4fe20bf4
lyday <dustin.lyday@gmail.com>
2017-01-20 08:11:47 -0600

    reanaming project

---
+++ workshop.migrations/Migrations/Migration0119171640TestFwdOnly.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace workshop.migrations.Migrations
+{
+    [Migration(3)]
+    public class Migration0119171640TestFwdOnly : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Ratings").WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("ParentalGuidanceRating").AsString(30);
+
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "NA"});
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "G"});
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "PG"});
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "PG-13"});
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "R"});
+            Insert.IntoTable("Ratings").Row(new {ParentalGuidanceRating = "X"});
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Ratings");
+        }
+    }
+}
